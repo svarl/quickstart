15f658fa5d4b62ffd9f9b6b42a28296bb4e97b40
Aleksandr Varlamov <aleksandr.varlamov@team.wrike.com>
2019-12-31 12:31:08 +0300

    catch problem with webdev

---
+++ web/app/app_component.dart
@@ -0,0 +1,15 @@
+import 'package:angular/angular.dart';
+
+@Component(
+  selector: 'my-app',
+  changeDetection: ChangeDetectionStrategy.Default,
+  styleUrls: ['app_component.css'],
+  templateUrl: 'app_component.html',
+)
+class AppComponent {
+
+
+
+
+
+}
